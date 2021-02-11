9408e367264fc79373aaf10ab4dcef234b6adc32
Mohammad Abdur Rahman <rahman.just16@gmail.com>
2021-02-11 23:41:14 +0600

    ts continue

--- typescript.ts
+++ typescript.ts
@@ -1,1 +1,21 @@
 console.log("hello");
+// let names:string ="hello world";
+// console.log(names);
+
+function add (num1:number , num2:number){
+    return num1 + num2;
+    
+    // return num1 + num2;
+}
+let v = add(1,2);
+// let x= add ('adam', 'sand');
+console.log(v);
+
+function doubleIt (num:number): void{
+    const result = num*2;
+    console.log(result);
+}
+doubleIt(10);
+
+const mul = (x:number,y:number):number => x*y; 
+console.log(mul(20,25));
